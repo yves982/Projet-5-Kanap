332beed0bc4e790dce2fcf703038192f6f3087a3
Romain BERNARDOT <Romain71.contact@gmail.com>
2022-06-24 12:43:41 +0200

    Ajout des détails des produits

--- front/javascript/script.js
+++ front/javascript/script.js
@@ -25,7 +25,7 @@ function lesKanaps(index) {
       /* création et ajout des zones d'articles, insertion de l'adresse produit via chemin produit + paramètres(son id);
       la page index est http://127.0.0.1:5500/front/html/index.html donc la page du produit sera http://127.0.0.1:5500/front/html/product.html 
       (d'ou le ./product.html) pour rajouter son paramètre on met ? puis la clé (ici _id) associé (=) à sa valeur dynamique ${article._id} */
-        zoneArticle.innerHTML += `<a href="./products.html?_id=${article._id}">
+        zoneArticle.innerHTML += `<a href="./product.html?_id=${article._id}">
         <article>
             <img src ="${article.imageUrl}" alt ="${article.altTxt}">
             <h3 class="productName">${article.name}</h3>
