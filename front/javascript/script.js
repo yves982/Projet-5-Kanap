2c531336847019de4809a7bd4fed761161f854f7
Yves LALANNE <yves.lalanne@azeo.com>
2023-08-06 13:37:57 +0200

    name variable properly (article).

--- front/javascript/script.js
+++ front/javascript/script.js
@@ -14,11 +14,12 @@
     //-------------------------------------------
     // Afficher les produits de l'api sur la page d'acceuil (index)
     //-------------------------------------------
-    function lesKanaps(index) {
+    function lesKanaps(articles) {
         // déclaration de variable de la zone d'article
         let zoneArticle = document.querySelector("#items");
+        
         // boucle pour chaque indice(nommé 'article') dans index
-        for (let article of index) {
+        for (let article of articles) {
             /* création et ajout des zones d'articles, insertion de l'adresse produit via chemin produit + paramètres(son id);
             la page index est http://127.0.0.1:5500/front/html/index.html donc la page du produit sera http://127.0.0.1:5500/front/html/product.html 
             (d'ou le ./product.html) pour rajouter son paramètre on met ? puis la clé (ici _id) associé (=) à sa valeur dynamique ${article._id} */
