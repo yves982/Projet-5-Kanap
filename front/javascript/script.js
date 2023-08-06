20066b4c3222d55f01cc30535342b2bb66a84929
Yves LALANNE <yves.lalanne@azeo.com>
2023-08-06 13:47:59 +0200

    - avoid to redefine DOM in a loop (rerender are costly).
    - use Array.map and Array.join to build a string based on array items.

--- front/javascript/script.js
+++ front/javascript/script.js
@@ -11,6 +11,18 @@
             console.error("Error" + err);
         });
     
+    function generateArticleHtml(article) {
+        return `<article>
+            <img src ="${article.imageUrl}" alt ="${article.altTxt}">
+            <h3 class="productName">${article.name}</h3>
+            <p class="productDescription">
+                ${article.description}
+            </p>
+            <!-- we should not access an underscore prefixed property by convention it used to mean private property -->
+            <a href="./product.html?_id=${article._id}">show more</a>
+        </article>`
+    }
+    
     //-------------------------------------------
     // Afficher les produits de l'api sur la page d'acceuil (index)
     //-------------------------------------------
@@ -18,19 +30,7 @@
         // déclaration de variable de la zone d'article
         let zoneArticle = document.querySelector("#items");
         
-        // boucle pour chaque indice(nommé 'article') dans index
-        for (let article of articles) {
-            /* création et ajout des zones d'articles, insertion de l'adresse produit via chemin produit + paramètres(son id);
-            la page index est http://127.0.0.1:5500/front/html/index.html donc la page du produit sera http://127.0.0.1:5500/front/html/product.html 
-            (d'ou le ./product.html) pour rajouter son paramètre on met ? puis la clé (ici _id) associé (=) à sa valeur dynamique ${article._id} */
-
-            zoneArticle.innerHTML += `<a href="./product.html?_id=${article._id}">
-        <article>
-            <img src ="${article.imageUrl}" alt ="${article.altTxt}">
-            <h3 class="productName">${article.name}</h3>
-            <p class="productDescription">${article.description}</p>
-        </article>
-    </a>`;
-        }
+        const articlesHtml = articles.map(generateArticleHtml)
+        zoneArticle.innerHTML = articlesHtml.join("\r\n")
     };
 })()
